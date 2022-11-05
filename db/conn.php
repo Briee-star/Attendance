f2eb9b48ec2703e7ce874ecbf673a18a3fd6a621
Briee-star <79051947+Briee-star@users.noreply.github.com>
2022-11-04 22:46:27 -0500

    Added remote my sql connections details

--- db/conn.php
+++ db/conn.php
@@ -1,8 +1,16 @@
 <?php 
-    $host = '127.0.0.1';
-    $db = 'attendance_db';
-    $user = 'root';
-    $pass = '';
+    //Development Connection
+    // $host = '127.0.0.1';
+    // $db = 'attendance_db';
+    // $user = 'root';
+    // $pass = '';
+    // $charset = 'utf8mb4';
+
+    //Remote Database Connection
+    $host = 'applied-web.mysql.database.azure.com';
+    $db = 'attendance_brittany';
+    $user = 'appliedweb_user@applied-web';
+    $pass = 'P@ssword1';
     $charset = 'utf8mb4';
 
     $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
